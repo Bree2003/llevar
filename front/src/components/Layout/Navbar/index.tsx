02d4771c80c477c76e5a55896eaf4214af6c7307
Bree2003 <breeale2003@gmail.com>
2026-09-02 12:52:53 -0400

    change button logout

--- front/src/components/Layout/Navbar/index.tsx
+++ front/src/components/Layout/Navbar/index.tsx
@@ -28,14 +28,98 @@ const Navbar = () => {
         <button
           type="button"
           onClick={() => navigate("/logout", { replace: true })}
-          className="flex flex-col items-center gap-1 max-w-[6rem] p-2 text-[--color-naranjo] transition-colors hover:bg-[--color-naranjo] hover:rounded-full hover:text-white"
           title={`${user.name} ${user.surname}`}
+          className="
+    group
+
+    flex
+    items-center
+    gap-3
+
+    max-w-[220px]
+
+    px-4
+    py-2
+
+    rounded-lg
+
+    border
+    border-white/20
+
+    text-white
+
+    hover:bg-white/10
+    hover:border-[--color-accent]
+
+    transition-all
+    duration-200
+  "
         >
-          {/* <Profile className="w-6 h-6 shrink-0" /> */}
-          <span className="w-full truncate text-center text-xs">
-            {`${user.name} ${user.surname}`}
-          </span>
-          <span className="w-full truncate text-center text-xs">(Logout)</span>
+          {/* USUARIO */}
+          <div
+            className="
+      min-w-0
+
+      flex
+      flex-col
+
+      text-left
+      leading-tight
+    "
+          >
+            <span
+              className="
+        truncate
+
+        text-sm
+        font-semibold
+      "
+            >
+              {`${user.name} ${user.surname}`}
+            </span>
+
+            <span
+              className="
+        text-xs
+
+        text-white/60
+
+        group-hover:text-[--color-accent]
+
+        transition-colors
+      "
+            >
+              Cerrar sesión
+            </span>
+          </div>
+
+          {/* ICONO LOGOUT */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth={2}
+            className="
+      w-5
+      h-5
+
+      flex-shrink-0
+
+      text-[--color-accent]
+
+      transition-transform
+      duration-200
+
+      group-hover:translate-x-0.5
+    "
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10 17l5-5-5-5M15 12H3M15 5h3a3 3 0 013 3v8a3 3 0 01-3 3h-3"
+            />
+          </svg>
         </button>
       </div>
     </nav>
